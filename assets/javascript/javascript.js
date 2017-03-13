15e2281d5bdb6037e60368d3d19fc13188e25a95
melissa gattoni <meligatt@gmail.com>
2017-03-13 22:42:24 +1100

    add DOM manipulation for robot class

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -20,35 +20,60 @@ class Robot {
   }
 
   place (x, y, f){
-    this.x = x;
-    this.y = y;
-    this.facing = f;
-    return (this.placed = true);
+    if ( this.board.validatePosition(this.x, this.y) && this._validateDirection(this.facing) ) {
+
+      // last x,y, facing
+      this.lastX = this.x;
+      this.lastY = this.y;
+      this.lastFacing = this.facing;
+
+      // new x, y , facing
+      this.x = x;
+      this.y = y;
+      this.facing = f;
+
+      this._cleanDOMPosition(true);
+      this.render();
+      return (this.placed = true);
+
+    } else {
+      return (this.placed = false);
+    }
   }
 
   move() {
     if (this.placed) {
+      this._cleanDOMPosition(false);
       if ( this.board.validatePosition(this.x, this.y) && this._validateDirection(this.facing) ) {
         switch (this.facing) {
           case 'NORTH':
-          this.y += 1;
+          if (!(this.y === 4)) {
+            this.y += 1;
+          }
           break;
           case 'SOUTH':
-          this.y -= 1;
+          if (!(this.y === 0)) {
+            this.y -= 1;
+          }
           break;
           case 'WEST':
-          this.x -= 1
+          if (!(this.x === 0)) {
+            this.x -= 1;
+          }
           break;
           case 'EAST':
-          console.log('case EAST');
-          this.x += 1
+          if (!(this.x === 4)) {
+            this.x += 1;
+          }
           break;
           default:
           return false;
         }
-      } else {
-        console.log(`position: [${this.x},${this.y + 1}] and facing: [${this.facing}] is not valid!`);
-      }
+          this.render();
+        } else {
+          console.log(`position: [${this.x},${this.y + 1}] and facing: [${this.facing}] is not valid!`);
+        }
+
     } else {
       console.log("A droid needs to be placed first!");
     }
@@ -94,6 +119,28 @@ class Robot {
     return true;
   }
 
+  render() {
+    console.log("render",this.x, this.y);
+    const row = document.querySelector(`.row${this.y}`);
+    const cell = row.querySelector(`.col${this.x}`);
+    cell.textContent = 'ROBOT HERE!';
+  }
+
+  _cleanDOMPosition(replaced){
+    // getting the class name for row and col element:
+    if (replaced === false) {
+    const row = document.querySelector(`.row${this.y}`);
+    const cell = row.querySelector(`.col${this.x}`);
+    cell.textContent = '';
+    console.log("replaced false", cell);
+  } else {
+    const row = document.querySelector(`.row${this.lastY}`);
+    const cell = row.querySelector(`.col${this.lastX}`);
+    cell.textContent = '';
+    console.log("replaced true ", cell);
+  }
+}
+
   report () {
     console.log(`Output: [${this.name}] is placed: [${this.placed}], at [${this.x},${this.y}] facing [${this.facing}]`);
   }
@@ -106,7 +153,7 @@ class Board {
   }
 
   validatePosition(x, y){
-    if (!(x <= this.size && y <= this.size)) {
+    if ( !(x >= 0 && y >= 0 && x <= this.size && y <= this.size) ) {
       return false;
     }
     return true;
@@ -116,8 +163,16 @@ class Board {
 // -----------------------------------------------------------------------------
 //creating an instance of the Robot class:
 const boardGame = new Board(5);
-const droid = new Robot("BB8", 2, 3, "NORTH", boardGame);
+const droid = new Robot("BB8", 0, 0, "SOUTH", boardGame);
 droid.move();
-droid.report();
-droid.place(3,4,'SOUTH');
+droid.left();
+droid.left();
+droid.move();
+droid.place(3,4,'EAST');
+droid.move();
+droid.move();
+droid.right();
+droid.move();
+droid.place(0,0,'EAST');
+
 droid.report();
