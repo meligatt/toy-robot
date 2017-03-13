15a52b5e91d0876c8a7829846e9464f845a468e3
melissa gattoni <meligatt@gmail.com>
2017-03-13 15:35:09 +1100

    validate move method

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -8,13 +8,14 @@
 // warning when there is no board to move
 
 class Robot {
-  constructor (name, x, y, f){
+  constructor (name, x, y, f, board){
     this.name = name;
     this.x = x;
     this.y = y;
     this.facing = f;
     this.placed= false;
     this.directions = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
+    this.board = board;
     this.init("hello", this.x, this.y, this.facing);
     return this;
   }
@@ -32,26 +33,26 @@ class Robot {
   }
 
   move() {
-    switch (this.facing) {
-      case 'NORTH':
-      this.y += 1;
-      // y++
-      break;
-      case 'SOUTH':
-      // y--
-      this.y -= 1;
-      break;
-      case 'WEST':
-      // x--
-      this.x -= 1
-      break;
-      case 'EAST':
-      // x++
-      this.x += 1
-      break;
-      default:
-      return false;
-    }
+    if ( this.board.validatePosition(this.x, this.y) && this.directions.includes(this.facing) ) {
+      switch (this.facing) {
+        case 'NORTH':
+             this.y += 1;
+          break;
+        case 'SOUTH':
+        this.y -= 1;
+        break;
+        case 'WEST':
+        this.x -= 1
+        break;
+        case 'EAST':
+        this.x += 1
+        break;
+        default:
+        return false;
+      }
+   } else {
+     console.log(`position: [${this.x},${this.y + 1}] and facing: [${this.facing}] is not valid!`);
+   }
   }
 
   left (){
@@ -80,16 +81,37 @@ class Robot {
     return element === this.facing;
   }
 
+  _validateDirection(f){
+    // todo
+  }
+
   report () {
     console.log(`[${this.name}] is placed: [${this.placed}], at [${this.x},${this.y}] facing [${this.facing}]`);
   }
 
 }
+
+class Board {
+  constructor(size){
+    this.size = size;
+  }
+
+  validatePosition(x, y){
+    if (!(x < this.size && y < this.size)) {
+      return false;
+    }
+    return true;
+  }
+}
+
 //creating an instance of the Robot class:
-const droid = new Robot("BB8", 2, 3, "NORTH");
+const boardGame = new Board(5);
+const droid = new Robot("BB8", 2, 3, "NORTH", boardGame);
 droid.report();
 droid.move();
 droid.move();
+droid.move();
+
 droid.report();
 droid.left();
 droid.left();
