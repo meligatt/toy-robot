73fe3177cecc020e604c443dbede2090b2eae6d3
melissa gattoni <meligatt@gmail.com>
2017-03-15 14:21:11 +1100

    add Mocha and chai tests for Board Class

--- assets/javascript/Board.js
+++ assets/javascript/Board.js
@@ -1,13 +1,24 @@
 class Board {
   constructor(size){
-    this.size = size;
+    this.MAX_SIZE = 5;
+    if(size > this.MAX_SIZE || size == null || size < this.MAX_SIZE ){
+      this.size = this.MAX_SIZE;
+    } else {
+        this.size = size;
+    }
   }
 
   validatePosition(x, y){
-    if ( !(x >= 0 && y >= 0 && x <= this.size && y <= this.size) ) {
+    if ( !(x >= 0 && x < this.size) ) {
       return false;
     }
+
+    if ( !(y >= 0 && y < this.size) ) {
+      return false;
+    }
+
     return true;
   }
 
 }
+module.exports = Board;
